f72c0c039fd44b358d3259180ce7e0a8ee401f8d
kanon-bee <kanon754@gmail.com>
2022-05-22 06:12:23 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.scss';
 import NameList from './Components/NameList/NameList';
 import InforCard from './Components/InfoCard/InfoCard';
 import { BrowserRouter, Route, Routes, useLocation, useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import data from './Data/Data';
 
 function App() {
 
+  const path = window.location.pathname.split('/')
+
+  console.log(path)
 
   return (
     <div className="App">
-      Hello world
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<NameList />}/>
-          {data.map(item => {
-            return <Route key={item.id} path={`/users/${item.id}`} element={<InforCard id={item.id}/>} />
-          })}
+          <Route path='/react-app' element={<NameList />}/>
+          <Route path="/users/:id" element={<InforCard />} />
         </Routes>
       </BrowserRouter>
     </div>
